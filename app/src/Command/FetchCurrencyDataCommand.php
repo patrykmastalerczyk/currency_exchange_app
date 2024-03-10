bdfb4ed55505badb5e7f04493b26cdf898101b5e
Patryk Mastalerczyk <patmast56@gmail.com>
2024-03-10 18:34:05 +0100

    Added logger for data displaying purposes, registered command

--- app/src/Command/FetchCurrencyDataCommand.php
+++ app/src/Command/FetchCurrencyDataCommand.php
@@ -5,23 +5,34 @@ namespace App\Command;
 use App\Dto\CurrencyDto;
 use App\Repository\CurrencyRepository;
 use App\Service\CurrencyService;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class FetchCurrencyDataCommand extends Command
 {
+    protected static $defaultName = 'app:fetch-currency-rates';
+
     public function __construct(
         private readonly CurrencyRepository $currencyRepository,
         private readonly CurrencyService $currencyService,
+        private readonly LoggerInterface $logger
     )
     {
         parent::__construct();
     }
 
+    protected function configure(): void
+    {
+        $this->setName(self::$defaultName)->setDescription('Fetch currency exchange rates from an external API.');
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $exchangeRates = $this->currencyService->fetchExchangeRates();
+
+        $this->logger->alert('Fetching currency exchange rates...');
 
         foreach ($exchangeRates as $exchangeRate) {
             $existingRate = $this->currencyRepository->findByName($exchangeRate->name);
@@ -30,6 +41,11 @@ class FetchCurrencyDataCommand extends Command
                 $existingRate?->getId() ?? Uuid::v4(),
                 new CurrencyDto($exchangeRate->name, $exchangeRate->currencyCode, $exchangeRate->exchangeRate)
             );
+
+            $this->logger->alert(sprintf('Currency %s updated. Current exchange rate is now: %f.',
+                $exchangeRate->currencyCode,
+                $exchangeRate->exchangeRate
+            ));
         }
 
         return Command::SUCCESS;
