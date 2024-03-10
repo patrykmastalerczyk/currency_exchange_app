8ee48e258598cf3fad1c018f589bcf12b943451b
Patryk Mastalerczyk <patmast56@gmail.com>
2024-03-10 18:37:06 +0100

    Created migrations, minor package changes

--- app/src/Command/FetchCurrencyDataCommand.php
+++ app/src/Command/FetchCurrencyDataCommand.php
@@ -9,6 +9,7 @@ use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Uid\Uuid;
 
 class FetchCurrencyDataCommand extends Command
 {
